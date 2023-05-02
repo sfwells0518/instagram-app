47e0b150ead9feb4b8491301fe2ab61d0180ead5
Sam Wells <sfwells0518@gmail.com>
2023-05-02 13:15:39 -0500

    add Profile model and profiles controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(
-      first_name: params[:user][:first_name],
-      last_name: params[:user][:last_name],
       email: params[:user][:email],
+      full_name: params[:user][:full_name],
       username: params[:user][:username],
       password: params[:user][:password],
       password_confirmation: params[:user][:password_confirmation],
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_02_174307) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_181315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "profile_picture"
+    t.text "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
